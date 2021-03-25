3d26d3a28780b33402ae64e9a4724b6a133e3d69
aria <aryashahbazi@gmail.com>
2021-03-26 00:22:02 +0430

    Toupper_ToLower
    Converts letters to uppercase and lowercase

--- app/main.cpp
+++ app/main.cpp
@@ -1,12 +1,47 @@
 
 #include <iostream>
-#include <stdlib.h>
+#include <cstring>
 
-#include "Foo.h"
+using namespace std;
+
+void Toupper(char n);
+void Tolower(char m);
+
 int main()
 {
-  Foo d = Foo();
-  std::cout<< d.doSomething()<<std::endl;
 
-  return 0;
+  Toupper('q');
+  Toupper('u');
+
+  Tolower('Q');
+  Tolower('U');
+
+}
+
+
+void Tolower(char m)
+{
+  if (m == ' ')
+  {
+    cout << 0;
+  }
+  else if(m>=65 && m<=90)
+  {
+    int b = tolower(m);
+    cout << (char)b << endl;
+  }
+}
+
+
+void Toupper(char n)
+{
+  if (n == ' ')
+  {
+    cout<< 0;
+  }
+  else if(n>=97 && n<= 122)
+  {
+    int d = toupper(n);
+    cout <<  (char)d << endl;
+  }
 }
