df1db10fd459bbc622dfb8d780a113a2e7effd5a
aria <aryashahbazi@gmail.com>
2021-03-26 00:24:40 +0430

    Done
    Chapter 3

---
+++ Build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: G:/C++/AW/Toupper_Tolower/Toupper_Tolower
+# Build directory: G:/C++/AW/Toupper_Tolower/Toupper_Tolower/Build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tests")
---
+++ Build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: G:/C++/AW/Toupper_Tolower/Toupper_Tolower/tests
+# Build directory: G:/C++/AW/Toupper_Tolower/Toupper_Tolower/Build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(myLibrary.unit_tests "G:/C++/AW/Toupper_Tolower/Toupper_Tolower/Build/unit_tests.exe")
+set_tests_properties(myLibrary.unit_tests PROPERTIES  _BACKTRACE_TRIPLES "G:/C++/AW/Toupper_Tolower/Toupper_Tolower/tests/CMakeLists.txt;22;add_test;G:/C++/AW/Toupper_Tolower/Toupper_Tolower/tests/CMakeLists.txt;0;")
